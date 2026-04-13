07e442bd83df4c4bb0f048f1f532550bc945ac06
Thu Nguyen <thuthien2001@gmail.com>
2026-04-13 16:22:32 +0700

    feat: update schema

--- daemon/src/database/schema.ts
+++ daemon/src/database/schema.ts
@@ -1,19 +1,25 @@
 import { sqliteTable } from 'drizzle-orm/sqlite-core';
 import * as t from 'drizzle-orm/sqlite-core';
 
-// const timestamps = {
-//   updatedAt: t.integer({ mode: 'timestamp' }),
-//   createdAt: t
-//     .integer({ mode: 'timestamp' })
-//     .notNull()
-//     .default(Temporal.now().toMillis()),
-// };
+// TODO: extract to a separate .helper file if this gets too big
+const timestamps = {
+  updatedAt: t.integer({ mode: 'timestamp' }),
+  createdAt: t.integer({ mode: 'timestamp' }).notNull(),
+};
 
 export const vaults = sqliteTable('vaults', {
   id: t.text().primaryKey(),
-  name: t.text().notNull(),
+  name: t.text().notNull().unique(), // Unique because the cli might be confused by multiple vaults with the same name
   localPath: t.text().notNull().unique(),
   remote: t.text().notNull(),
   branch: t.text().notNull().default('main'),
+  isDirty: t.integer({ mode: 'boolean' }).notNull().default(false),
   autoSync: t.integer({ mode: 'boolean' }).notNull().default(false),
+  syncInterval: t
+    .integer()
+    .notNull()
+    .default(5 * 60), // in seconds
+  conflictStrategy: t.text().notNull().default('log-and-skip'),
+  lastSyncedAt: t.integer({ mode: 'timestamp' }),
+  ...timestamps,
 });
