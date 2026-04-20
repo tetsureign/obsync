036dbc936c9cd06eb178aa4fd4880dc50dce0378
Thu Nguyen <thuthien2001@gmail.com>
2026-04-20 15:23:06 +0700

    chore: remove redundant service file and add a controller file

--- daemon/src/vault/vault.module.ts
+++ daemon/src/vault/vault.module.ts
@@ -1,17 +1,16 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from '@/database/database.module';
-import { VaultService } from './vault.service';
 import { CreateVaultHandler } from './commands/create-vault.command';
 import { VaultRepository } from './vault.repository';
 import { UpdateVaultHandler } from './commands/update-vault.command';
 import { DeleteVaultHandler } from './commands/delete-vault.command';
 import { GetVaultHandler } from './queries/get-vault.query';
 import { ListVaultsHandler } from './queries/list-vaults.query';
+import { VaultController } from './vault.controller';
 
 @Module({
   imports: [DatabaseModule],
   providers: [
-    VaultService,
     VaultRepository,
     CreateVaultHandler,
     UpdateVaultHandler,
@@ -19,5 +18,6 @@ import { ListVaultsHandler } from './queries/list-vaults.query';
     GetVaultHandler,
     ListVaultsHandler,
   ],
+  controllers: [VaultController],
 })
 export class VaultModule {}
--- daemon/src/vault/vault.service.ts
+++
@@ -1,7 +0,0 @@
-import { Database } from '@/database/database';
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class VaultService {
-  constructor(private readonly database: Database) {}
-}
