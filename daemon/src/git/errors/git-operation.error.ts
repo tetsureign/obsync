18cab7d3520436db0e68c86f6bba056301c097fb
Thu Nguyen <thuthien2001@gmail.com>
2026-05-05 16:24:02 +0700

    refactor: more generalized git operations

--- daemon/src/git/errors/git-operation.error.ts
+++ daemon/src/git/errors/git-operation.error.ts
@@ -2,7 +2,7 @@ import { AppError } from '@/common/errors/app.error';
 
 export class GitOperationError extends AppError {
   constructor(errorMessage: string) {
-    super(errorMessage, 'GIT_OPERATION_ERROR', 500, {
+    super(errorMessage, 'GENERAL_GIT_OPERATION_ERROR', 500, {
       errorMessage,
     });
   }
--- daemon/src/git/git.adapter.ts
+++ daemon/src/git/git.adapter.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { simpleGit, GitError } from 'simple-git';
+import { simpleGit, GitError, SimpleGit } from 'simple-git';
 import { NotAGitRepoError } from './errors/not-git-repo.error';
 import { RemoteMismatchError } from './errors/remote-mismatch.error';
 import { MergeConflictError } from './errors/merge-conflict.error';
@@ -14,59 +14,78 @@ export class GitAdapter {
   private readonly networkErrorString = 'Could not resolve host';
 
   async assertValidVault(localPath: string, expectedRemoteUrl: string) {
-    const git = simpleGit({ baseDir: localPath });
-    const isRepo = await git.checkIsRepo();
-    if (!isRepo) {
-      throw new NotAGitRepoError(localPath);
-    }
+    await this.runGitOperation(localPath, async (git) => {
+      const isRepo = await git.checkIsRepo();
+      if (!isRepo) {
+        throw new NotAGitRepoError(localPath);
+      }
 
-    const remotes = await git.getRemotes(true);
-    const origin = remotes.find((remote) => remote.name === 'origin');
-    if (origin?.refs.push !== expectedRemoteUrl) {
-      throw new RemoteMismatchError(
-        localPath,
-        expectedRemoteUrl,
-        origin?.refs.push,
-      );
-    }
+      const remotes = await git.getRemotes(true);
+      const origin = remotes.find((remote) => remote.name === 'origin');
+      if (origin?.refs.push !== expectedRemoteUrl) {
+        throw new RemoteMismatchError(
+          localPath,
+          expectedRemoteUrl,
+          origin?.refs.push,
+        );
+      }
+    });
   }
 
   async getStatus(localPath: string) {
-    const git = simpleGit({ baseDir: localPath });
-    return await git.status();
+    return await this.runGitOperation(localPath, async (git) => {
+      return await git.status();
+    });
   }
 
   async pull(localPath: string) {
-    try {
-      const git = simpleGit({ baseDir: localPath });
+    await this.runGitOperation(localPath, async (git) => {
       await git.pull();
-    } catch (error) {
-      if (error instanceof GitError) {
-        if (error.message.includes(this.conflictErrorString))
-          throw new MergeConflictError(localPath);
-        if (error.message.includes(this.authenticationErrorString))
-          throw new RemoteAuthError(localPath);
-        if (error.message.includes(this.networkErrorString))
-          throw new NetworkError(localPath);
-
-        throw new GitOperationError(error.message);
-      }
-      throw error;
-    }
+    });
   }
 
   async stage(localPath: string, filePaths: string[]) {
-    const git = simpleGit({ baseDir: localPath });
-    await git.add(filePaths);
+    await this.runGitOperation(localPath, async (git) => {
+      await git.add(filePaths);
+    });
   }
 
   async commit(localPath: string, message: string) {
-    const git = simpleGit({ baseDir: localPath });
-    await git.commit(message);
+    await this.runGitOperation(localPath, async (git) => {
+      await git.commit(message);
+    });
   }
 
   async push(localPath: string) {
-    const git = simpleGit({ baseDir: localPath });
-    await git.push();
+    await this.runGitOperation(localPath, async (git) => {
+      await git.push();
+    });
+  }
+
+  private async runGitOperation<T>(
+    localPath: string,
+    operation: (git: SimpleGit) => Promise<T>,
+  ): Promise<T> {
+    try {
+      const git = simpleGit({ baseDir: localPath });
+      return await operation(git);
+    } catch (error) {
+      this.handleGitError(localPath, error);
+    }
+  }
+
+  private handleGitError(localPath: string, error: unknown): never {
+    if (error instanceof GitError) {
+      if (error.message.includes(this.conflictErrorString))
+        throw new MergeConflictError(localPath);
+      if (error.message.includes(this.authenticationErrorString))
+        throw new RemoteAuthError(localPath);
+      if (error.message.includes(this.networkErrorString))
+        throw new NetworkError(localPath);
+
+      throw new GitOperationError(error.message);
+    }
+
+    throw error;
   }
 }
