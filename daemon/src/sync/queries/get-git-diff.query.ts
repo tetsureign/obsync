104d5d2cfa0d526bdea51db4783eeb2f7191e3d7
Thu Nguyen <thuthien2001@gmail.com>
2026-05-11 20:45:34 +0700

    fix: git diff & git status query being plainly wrong

--- daemon/src/sync/queries/get-git-diff.query.ts
+++ daemon/src/sync/queries/get-git-diff.query.ts
@@ -1,5 +1,8 @@
 import { IQueryHandler, QueryHandler } from '@nestjs/cqrs';
 import { GitService } from '@/git/git.service';
+import { forwardRef, Inject } from '@nestjs/common';
+import { VaultRepository } from '@/vault/vault.repository';
+import { VaultNotFoundError } from '@/vault/errors/vault-not-found.error';
 
 export class GetGitDiffQuery {
   constructor(
@@ -10,9 +13,24 @@ export class GetGitDiffQuery {
 
 @QueryHandler(GetGitDiffQuery)
 export class GetGitDiffHandler implements IQueryHandler<GetGitDiffQuery> {
-  constructor(private readonly gitService: GitService) {}
+  constructor(
+    private readonly gitService: GitService,
+    @Inject(forwardRef(() => VaultRepository))
+    private readonly vaultRepository: VaultRepository,
+  ) {}
 
   async execute(query: GetGitDiffQuery) {
+    const vaultInfo = await this.vaultRepository.findById(query.vaultId);
+
+    if (!vaultInfo) {
+      throw new VaultNotFoundError(query.vaultId);
+    }
+
+    await this.gitService.assertValidVault(
+      vaultInfo.localPath,
+      vaultInfo.remote,
+    );
+
     return this.gitService.diff(query.vaultId, query.filePaths);
   }
 }
--- daemon/src/sync/queries/get-git-status.query.ts
+++ daemon/src/sync/queries/get-git-status.query.ts
@@ -1,5 +1,8 @@
 import { IQueryHandler, QueryHandler } from '@nestjs/cqrs';
 import { GitService } from '@/git/git.service';
+import { VaultRepository } from '@/vault/vault.repository';
+import { forwardRef, Inject } from '@nestjs/common';
+import { VaultNotFoundError } from '@/vault/errors/vault-not-found.error';
 
 export class GetGitStatusQuery {
   constructor(public readonly vaultId: string) {}
@@ -7,9 +10,24 @@ export class GetGitStatusQuery {
 
 @QueryHandler(GetGitStatusQuery)
 export class GetGitStatusHandler implements IQueryHandler<GetGitStatusQuery> {
-  constructor(private readonly gitService: GitService) {}
+  constructor(
+    private readonly gitService: GitService,
+    @Inject(forwardRef(() => VaultRepository))
+    private readonly vaultRepository: VaultRepository,
+  ) {}
 
   async execute(query: GetGitStatusQuery) {
+    const vaultInfo = await this.vaultRepository.findById(query.vaultId);
+
+    if (!vaultInfo) {
+      throw new VaultNotFoundError(query.vaultId);
+    }
+
+    await this.gitService.assertValidVault(
+      vaultInfo.localPath,
+      vaultInfo.remote,
+    );
+
     return await this.gitService.getStatus(query.vaultId);
   }
 }
