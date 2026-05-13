674eca752c319721d870bb1492e7fe7ed6478a5a
Thu Nguyen <thuthien2001@gmail.com>
2026-05-13 14:37:51 +0700

    feat: one queue per vault

--- daemon/src/sync-queue/sync-queue.module.ts
+++ daemon/src/sync-queue/sync-queue.module.ts
@@ -1,4 +1,8 @@
 import { Module } from '@nestjs/common';
+import { SyncQueue } from './sync-queue';
 
-@Module({})
+@Module({
+  providers: [SyncQueue],
+  exports: [SyncQueue],
+})
 export class SyncQueueModule {}
--- daemon/src/sync-queue/sync-queue.ts
+++ daemon/src/sync-queue/sync-queue.ts
@@ -4,15 +4,30 @@ import pRetry from 'p-retry';
 
 @Injectable()
 export class SyncQueue {
-  private queue = new PQueue({ concurrency: 1 });
+  private queues = new Map<string, PQueue>();
 
-  add(fn: () => Promise<void>) {
-    return this.queue.add(async () => {
-      await pRetry(fn, {
-        retries: 3,
-        minTimeout: 1000,
-        factor: 5,
-      });
-    });
+  getQueue(vaultId: string) {
+    if (!this.queues.has(vaultId)) {
+      this.queues.set(vaultId, new PQueue({ concurrency: 1 }));
+    }
+    return this.queues.get(vaultId)!;
+  }
+
+  async addToVaultQueue(
+    vaultId: string,
+    fn: () => Promise<void>,
+    priority = 0,
+  ) {
+    const queue = this.getQueue(vaultId);
+    return queue.add(
+      async () => {
+        await pRetry(fn, {
+          retries: 3,
+          minTimeout: 1000,
+          factor: 5,
+        });
+      },
+      { priority },
+    );
   }
 }
